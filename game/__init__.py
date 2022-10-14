a2d75fca7f25c77516602d9dd205699f369b2ddb
Pedro Morales <103544215+pmorales01@users.noreply.github.com>
2022-10-13 19:01:31 -0700

    Create __init__.py

---
+++ game/__init__.py
@@ -0,0 +1,13 @@
+# Pedro Morales
+# CPSC 386-03
+# 2022-04-23
+# pedrom2@csu.fullerton.edu
+# @pedromorales451
+#
+# Lab 05-00
+#
+# This file defines an __all__ variable for the Bouncing Balls game package.
+#
+"""Init file for the boxes PyGame demo."""
+
+__all__ = ["game", "rgbcolors", "scene"]
